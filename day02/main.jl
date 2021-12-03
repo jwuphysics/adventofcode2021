e5478d9c6da49773ed206e38a0ff521985d657a7
John Wu <jwuphysics@gmail.com>
2021-12-03 09:41:02 -0500

    debugged and ready to go

--- day02/main.jl
+++ day02/main.jl
@@ -3,22 +3,33 @@ function readInput(path::String)
 end
 
 directions = Dict{String,Array}(
-    "forward" => [1,0],
-    "back"    => [-1,0],
-    "up"      => [0,-1],
-    "down"    => [0,1]
+    "forward" => [1 0],
+    "back"    => [-1 0],
+    "up"      => [0 -1],
+    "down"    => [0 1]
 )
 
 function part1(input)
-    start = [0,0]
+    position = [0 0]
     moves = [directions[split(s)[1]] * parse(Int, split(s)[2]) for s in input]
-    return prod(start + sum(moves))
+    return prod(position + sum(moves))
+end
+
+function part2(input)
+    position = [0 0]
+    aim = 0
+    for s in input
+        (x,y) = directions[split(s)[1]] * parse(Int, split(s)[2])
+        aim += y
+        position += [x x*aim]
+    end
+    return prod(position)
 end
 
 function main()
     input = readInput("input.txt")
     input = split(input, "\n")
-    println(part1(input))
+    println(part1(input), " ", part2(input))
 end
 
 main()
